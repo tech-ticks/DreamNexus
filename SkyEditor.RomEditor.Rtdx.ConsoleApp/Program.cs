254dd882b91daf662f6987c6ff4c6ff752db8f9f
Dixon, Evan <evanldixon@gmail.com>
2020-04-23 17:15:06 -0500

    Allow multiple lua scripts to run in the same context

--- SkyEditor.RomEditor.Rtdx.ConsoleApp/Program.cs
+++ SkyEditor.RomEditor.Rtdx.ConsoleApp/Program.cs
@@ -11,28 +11,77 @@ namespace SkyEditor.RomEditor.Rtdx.ConsoleApp
     {
         static void PrintUsage()
         {
-            Console.WriteLine("SkyEditor.RomEditor.Rtdx.Console <RomDirectory> <Script.lua> [--save]");
+            Console.WriteLine("Usage: ");
+            Console.WriteLine("dotnet SkyEditor.RomEditor.Rtdx.Console.dll <RomDirectory> <Script1.lua> [Script2.lua] [...] [--save | --save-to <RomDirectory>]");
+            Console.WriteLine();
+            Console.WriteLine("Examples: ");
+            Console.WriteLine("dotnet SkyEditor.RomEditor.Rtdx.Console.dll ./RTDX ./Scripts/Queries/ListStarters.lua");
+            Console.WriteLine("dotnet SkyEditor.RomEditor.Rtdx.Console.dll ./RTDX ./Scripts/Samples/ChangeStarters.lua --save");
+            Console.WriteLine("dotnet SkyEditor.RomEditor.Rtdx.Console.dll ./RTDX ./Scripts/Samples/ChangeStarters.lua --save-to ./RTDX-modified");
+            Console.WriteLine("dotnet SkyEditor.RomEditor.Rtdx.Console.dll ./RTDX ./Scripts/Samples/ChangeStarters.lua ./Scripts/Hypothetical/ChangeMoreStarters.lua --save-to ./RTDX-modified");
+            Console.WriteLine("dotnet SkyEditor.RomEditor.Rtdx.Console.dll ./RTDX ./Scripts/Queries/ListStarters.lua ./RTDX-Copy ./Scripts/Samples/ChangeStarters.lua ./Scripts/Hypothetical/ChangeMoreStarters.lua --save");
         }
 
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length < 2 || string.Equals(args[0], "--help", StringComparison.OrdinalIgnoreCase))
             {
                 PrintUsage();
                 return;
             }
 
-            var romDirectory = args[0];
-            var scriptFilename = args[1];
-            var saveRom = args.Contains("--save", StringComparer.OrdinalIgnoreCase);
-
-            var rom = new RtdxRom(romDirectory, PhysicalFileSystem.Instance);
-            var luaContext = new SkyEditorLuaContext(rom);
-            luaContext.Execute(File.ReadAllText(scriptFilename));
-
-            if (saveRom)
+            var fileSystem = PhysicalFileSystem.Instance;
+            RtdxRom? rom = null;
+            SkyEditorLuaContext? context = null;
+            for (int i = 0; i < args.Length; i++)
             {
-                rom.Save();
+                if (string.Equals(args[i], "--save", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (rom == null)
+                    {
+                        throw new InvalidOperationException("Argument '--save' must follow a ROM directory argument");
+                    }
+                    rom.Save();
+                    Console.WriteLine("Saved");
+                }
+                else if (string.Equals(args[i], "--save-to", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (rom == null)
+                    {
+                        throw new InvalidOperationException("Argument '--save-to' must follow a ROM directory argument");
+                    }
+                    if (i + 1 < args.Length)
+                    {
+                        throw new ArgumentException("Argument '--save-to' must be followed by a ROM directory argument");
+                    }
+                    rom.Save(args[i + 1], fileSystem);
+                    Console.WriteLine("Saved to " + args[i + 1]);
+                }
+                else if (Directory.Exists(args[i]))
+                {
+                    rom = new RtdxRom(args[i], fileSystem);
+                    context = new SkyEditorLuaContext(rom);
+                    Console.WriteLine("Loaded " + args[i]);
+                }
+                else if (File.Exists(args[i]))
+                {
+                    var extension = Path.GetExtension(args[i]);
+                    if (string.Equals(extension, ".lua", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (context == null)
+                        {
+                            throw new InvalidOperationException("ROM directory argument must precede Lua script argument");
+                        }
+
+                        Console.WriteLine(args[i] + ": ");
+                        context.Execute(File.ReadAllText(args[i]));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Unrecognized argument '{args[i]}'");
+                    return;
+                }
             }
         }
     }
