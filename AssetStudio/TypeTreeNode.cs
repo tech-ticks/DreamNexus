577a7d93501a4adb47e09bd2a6d747afe3736e34
Dixon, Evan <evanldixon@gmail.com>
2020-04-29 11:25:49 -0500

    Add AssetStudio

---
+++ AssetStudio/TypeTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssetStudio
+{
+    public class TypeTreeNode
+    {
+        public string m_Type;
+        public string m_Name;
+        public int m_ByteSize;
+        public int m_Index;
+        public int m_IsArray; //m_TypeFlags
+        public int m_Version;
+        public int m_MetaFlag;
+        public int m_Level;
+        public uint m_TypeStrOffset;
+        public uint m_NameStrOffset;
+        public ulong m_RefTypeHash;
+    }
+}
