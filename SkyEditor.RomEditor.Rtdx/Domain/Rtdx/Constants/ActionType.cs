35672d3455c3fce03bb574cb91e76b7e8b2ac036
StrikerX3 <ivan.rober@gmail.com>
2021-01-16 00:28:11 -0300

    More progress figuring out the actions

--- SkyEditor.RomEditor.Rtdx/Domain/Rtdx/Constants/ActionType.cs
+++ SkyEditor.RomEditor.Rtdx/Domain/Rtdx/Constants/ActionType.cs
@@ -260,13 +260,13 @@
 
         // Reverse stat changes (making positive changes negative and vice-versa) (Topsy-Turvy)
         ReverseStatChanges = 55,
-        
+
         // Swap raw Attack stat with raw Defense state (Power Trick)
         SwapAttackWithDefense = 56,
-        
+
         // Copy the target's stat changes (Psych Up)
         CopyTargetStatChanges = 57,
-        
+
         // Raise a random stat by two stages (Acupressure)
         //   Params[0] = (percentage) Chance to apply stat change?
         //   Params[5] = (unknown)
@@ -278,7 +278,7 @@
 
         // Reset positive Evasion stat changes to zero
         ResetPositiveEvasion = 60,
-        
+
         // Swap Attack and Special Attack boosts and multipliers between attacker and target (Power Swap)
         SwapAttackAndSpAttackBoosts = 61,
 
@@ -294,18 +294,18 @@
         // Average Defense and Special Defense stats, replacing both attacker's and target's stats (Guard Split)
         AverageDefenseAndSpecialDefense = 65,
 
-        // Apply status effect
+        // Chance to apply status effect
         //   Params[0] = (percentage) Chance to apply stat changes
         //   Params[5] = (StatusIndex) Status effect to apply
         // Bit 10 of action flags indicate if the target is user (when set) or target (when clear)
-        ApplyStatusEffect = 66,
+        ApplyStatusEffectWithChance = 66,
 
-        // Apply status effect
+        // Chance to apply status effect, with an additional unknown parameter
         //   Params[0] = (percentage) Chance to apply status effect
         //   Params[3] = (unknown)
         //   Params[4] = (StatusIndex) Status effect to apply
         // Bit 10 of action flags indicate if the target is user (when set) or target (when clear)
-        ApplyStatusEffect2 = 67,
+        ApplyStatusEffectWithChanceAndSomethingElse = 67,
 
         // Apply freeze, burn or paralysis (Tri Attack)
         //   Params[0] = (percentage) Chance to apply status effect
@@ -327,13 +327,260 @@
 
         // Heal all negative status effects and remove seals
         HealAllNegativeStatusEffects = 71,
-        
-        // TODO: investigate 72 through 79
+
+        // Seal the last move used by the target
+        SealLastUsedMove = 72,
+
+        // Seal all moves from enemies that the user also knows
+        SealMovesKnownByUser = 73,
+
+        // Transfer all negative status effects to the target and unseals all moves
+        TransferNegativeStatusEffectsToTarget = 74,
+
+        // Grant Protect to user and all teammates; duration depends on team size
+        AllProtect = 75,
+
+        // TODO: investigate 76 -- used by action 796, which is not used by any moves or items
+        //   Params[0] = (percent) Unknown purpose
+
+        // TODO: investigate 77 -- used by action 797, which is not used by any moves or items
+
+        // Throw the target to another Pokémon (Seismic Toss)
+        ThrowTargetAtOther = 78,
+
+        // Puts the target to Sleep if not sleeping already.
+        // If target is sleeping, applies Terrified on NPCs or Puppet on the player.
+        Hypnosis = 79,
 
         // Blow away target up to 10 tiles; cause 5 damage on collision
         BlowAwayTarget = 80,
 
-        // TODO: investigate 81 through 179
+        // TODO: investigate 81: used by Pounce Wand (237 81 237 238)
+
+        // Warp to a random place on the same floor
+        Warp = 82,
+
+        // TODO: investigate 83: used by action 769, which is not used by any moves or items
+
+        // Steal target's item (Thief, Covet)
+        Steal = 84,
+
+        // Part two of Thief and Covet (unknown effect)
+        //   Params[1] = (unknown)  (15 in both moves)
+        ThiefCovetPartTwo = 85,
+
+        // Warp and surround target by teleporting the target one square in front of the user and teleporting all teammates around it
+        WarpAndSurround = 86,
+
+        // Decrease target's belly. Will not reduce belly below zero.
+        //   Params[0] = (percentage) Chance to reduce belly
+        //   Params[1] = (integer) Amount of belly to reduce
+        DecreaseBelly = 87,
+
+        // Restore HP equal to a percentage of maximum HP
+        //   Params[1] = (percentage) Percentage of HP to restore
+        RestoreHPPercent = 88,
+
+        // Fully heal HP and remove all negative status conditions
+        RestoreFullHPAndHealAllNegativeStatusEffects = 89,
+
+        // Fully heal HP and PP and remove all negative status conditions
+        RestoreFullHPAndPPAndHealAllNegativeStatusEffects = 90,
+
+        // Deal damage to creatures surrounding the target (Flame Burst)
+        //   Params[1] = (percentage) Damage dealt equal to percentage of max HP
+        DealDamageToSurroundingTargets = 91,
+
+        // Apply the Grounded status effect
+        //   Params[0] = (percentage) Chance to apply status effect
+        ApplyGrounded = 92,
+
+        // Unknown secondary effect applied by Smack Down
+        // (perhaps this is used to cancel Fly, Bounce and Sky Drop?)
+        SmackDownSecondaryEffect = 93,
+
+        // Jump over the target until the next square not occupied by a creature in a straight line
+        SplashBounce = 94,
+
+        // Transform into the target, copying their appearance, types, ability, moves and stat modifications
+        Transform = 95,
+
+        // TODO: confirm this
+        // Cause all effects of the action to only apply to targets that have the Plus or Minus ability (Magnetic Flux)
+        PlusAndMinusSelect = 96,
+
+        // TODO: confirm this
+        // Finishes the Magnetic Flux move
+        FinishMagneticFlux = 97,
+
+        // TODO: confirm this
+        // Cause all effects of the action to only apply to targets of the specified type (Rototiller, Flower Shield)
+        //   Params[1] = (boolean) Unknown purpose, (possibly Grounded only (1) or all (0))
+        //   Params[6] = (PokemonType) Type of Pokémon to affect
+        TypeSelect = 98,
+
+        // TODO: confirm this
+        // Finish the TypeSelect action (Flower Shield)
+        // (possibly all targets)
+        FinishTypeSelect1 = 99,
+
+        // TODO: confirm this
+        // Finish the TypeSelect action (Rototiller)
+        // (possibly only Grounded targets)
+        FinishTypeSelect2 = 100,
+
+        // TODO: confirm this
+        // Cause all effects of the action to only apply to targets affected by the specified status effect
+        StatusSelect = 101,
+        //   Params[3] = (boolean) Unknown purpose
+        //   Params[4] = (StatusIndex) Status effect
+
+        // TODO: confirm this
+        // Cause all effects of the action to only apply to targets affected by the specified status effect
+        StatusSelect2 = 102,
+        //   Params[3] = (boolean) Unknown purpose
+        //   Params[4] = (StatusIndex) Status effect
+
+        // Spend HP equal to a percentage of max HP
+        //   Params[1] = (percentage) Percentage of max HP to spend
+        SpendHP = 103,
+
+        // 104 is unused
+
+        // Cause the action to fail if the belly is empty
+        CheckBellyNotEmpty = 105,
+
+        // 106 is unused
+
+        // Reduce PP of target's last used move
+        //   Params[1] = (integer) Amount of PP to reduce
+        ReducePP = 107,
+
+        // Add a type to the target
+        //   Params[6] = (PokemonType) Type to add
+        AddType = 108,
+
+        // TODO: investigate if there is a hidden parameter to add a second type
+        // Change target's type
+        //   Params[6] = (PokemonType) Type to change to
+        ChangeType = 109,
+
+        // Copy the target's type
+        CopyType = 110,
+
+        // Set user's type to its first move's type
+        ChangeToFirstMoveType = 111,
+
+        // Set user's type to one that has defensive advantage against the target's last used move
+        ChangeToTypeWithAdvantageOverLastMove = 112,
+
+        // Set user's type based on the dungeon
+        ChangeTypeBasedOnDungeon = 113,
+
+        // TODO: confirm this
+        // Remove Flying type temporarily
+        RemoveFlyingType = 114,
+
+        // Switch places with the teammate directly behind user
+        //   Params[0] = (percentage?) Chance to switch places?
+        SwitchWithTeammateBehind = 115,
+
+        // Apply the specified dungeon status
+        //   Params[0] = (percentage) Change to apply status
+        //   Params[8] = (DungeonStatusIndex) Dungeon status to apply
+        ApplyDungeonStatus = 116,
+
+        // Use a random move (Metronome)
+        UseRandomMove = 117,
+
+        // Use the last move used by the target (Mirror Move, Copycat)
+        UseLastMove = 118,
+
+        // Use a random move from a teammate (Assist)
+        UseRandomTeammateMove = 119,
+
+        // Use a random move from the enemy (Me First)
+        UseRandomEnemyMove = 120,
+
+        // TODO: confirm this
+        // Use a random known move (Sleep Talk)
+        UseRandomKnownMove = 121,
+
+        // Use a move based on the current terrain (Nature Power)
+        UseMoveBasedOnTerrain = 122,
+
+        // Copy target's last used move (Mimic)
+        CopyLastUsedMove = 123,
+
+        // Replace move with target's last used move (Sketch)
+        ReplaceWithLastUsedMove = 124,
+
+        // TODO: investigate this; used by actions 674 and 675 (second "versions" of Snore and Sleep Talk)
+        Unknown125 = 125,
+
+        // Set a Spikes Trap where the user stands
+        SetSpikesTrap = 126,
+
+        // Set a Toxic Spikes Trap where the user stands
+        SetToxicSpikesTrap = 127,
+
+        // Set a Stealth Rock Trap where the user stands
+        SetStealthRockTrap = 128,
+
+        // Apply status effect
+        //   Params[4] = (StatusIndex) Status effect to apply
+        // Bit 10 of action flags indicate if the target is user (when set) or target (when clear)
+        ApplyStatusEffect = 129,
+
+        // Set user's and target's HP to the average of both HPs. Excess HP above max is lost.
+        //   Param[0] = (percentage) Chance to apply effect
+        SetHPToAverage = 130,
+
+        // Switch places between user and target
+        SwitchPlaces = 131,
+
+        // Teleports all teammates around the target
+        //   Params[1] = (boolean?) Unknown purpose
+        //   Params[2] = (integer?) Unknown purpose
+        //   Params[3] = (boolean?) Unknown purpose
+        SurroundTarget = 132,
+
+        // TODO: investigate 133 through 138
+        // Increase stockpile count
+        //   Params[1] = (integer) Amount to increase
+        Stockpile = 133,
+
+        // Deal damage based on stockpile count
+        //   Params[0] = (percentage) Critical hit ratio
+        SpitUp = 134,
+
+        // Heal HP based on stockpile count
+        //   Params[1] = (percentage) Percentage of max HP to heal per stockpile count
+        Swallow = 135,
+
+        // Reset stockpile count to zero
+        ClearStockpile = 136,
+
+        // Set user's HP to a percentage of max HP, regardless of the target
+        //   Params[1] = (percentage) Percentage of max HP to set user's current HP to. Will not reduce HP below 1.
+        SetUserHPToPercentageOfMaximum = 137,
+
+        // Take percentage of max HP as damage if user's type is Ghost (Curse)
+        //   Params[1] = (percentage) Percentage of max HP to take damage
+        TakeDamageIfGhost = 138,
+
+        // Set target's HP to a percentage of current HP
+        //   Params[1] = (percentage) Percentage of current HP to set user's current HP to. Will not reduce HP below 1.
+        MultiplyHPByPercentage = 139,
+
+        // Set target's HP to a percentage of max HP
+        //   Params[1] = (percentage) Percentage of max HP to set user's current HP to. Will not reduce HP below 1.
+        SetTargetHPToPercentageOfMaximum2 = 140,
+
+        // Warp user and set user's HP to one
+        WarpAndSetHPToOne = 141,
+        
+        // TODO: investigate 142 through 179
 
         // Restore belly
         //   Params[1] = (integer) Amount of belly to restore
@@ -351,20 +598,26 @@
 
         // TODO: investigate 183 through 189
 
-        // 190 = Increase max HP
+        // Increase max HP
         //   Params[2] = (integer) Amount of max HP to increase
-        IncreaseMaxHP = 182,
+        IncreaseMaxHP = 190,
 
         // TODO: investigate 191 through 221
 
-        // 222 = Heal HP and increase max HP until the threshold; increase max HP further if HP is at max above the threshold
+        // Heal HP and increase max HP until the threshold; increase max HP further if HP is at max above the threshold
         //   Params[1] = (integer) Amount of HP to restore
         //   Params[2] = (integer) Amount of max HP to increase
         //   Params[3] = (integer) HP threshold
         //                - The max HP will always be increased if below the threshold,
         //                  otherwise max HP will only be increased if user is at full HP
-        RestoreAndIncreaseHP = 182,
+        RestoreAndIncreaseHP = 222,
 
-        // TODO: investigate 223 through 258
+        // TODO: investigate 223 through 235
+
+        // TODO: investigate 236: used by Switcher Wand (236 131 236 238)
+        // TODO: investigate 237: used by Pounce Wand (237 81 237 238)
+        // TODO: investigate 238: used by Switcher Wand and Pounce Wand
+
+        // TODO: investigate 239 through 258
     }
 }
