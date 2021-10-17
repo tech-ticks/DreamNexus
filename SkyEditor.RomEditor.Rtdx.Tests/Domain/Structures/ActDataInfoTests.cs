74af6ebaaa734a8eeb38269ce26995231f98e4ae
StrikerX3 <ivan.rober@gmail.com>
2021-10-17 12:46:22 -0300

    Add unit test to ensure ActDataInfo entries are rebuilt perfectly

---
+++ SkyEditor.RomEditor.Rtdx.Tests/Domain/Structures/ActDataInfoTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using SkyEditor.IO.Binary;
+using SkyEditor.RomEditor.Domain.Rtdx.Structures;
+using Xunit;
+
+namespace SkyEditor.RomEditor.Tests.Domain.Structures
+{
+    public class ActDataInfoTests
+    {
+        [Fact]
+        public void CanRebuildBytePerfectEntry()
+        {
+            // Arrange
+            var data = new byte[0xA0];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)i;
+            }
+            var entry = new ActDataInfo.Entry(new BinaryFile(data));
+
+            // Act
+            var bin = entry.ToBytes().ToArray();
+
+            // Assert
+            bin.Should().BeEquivalentTo(data);
+        }
+    }
+}
