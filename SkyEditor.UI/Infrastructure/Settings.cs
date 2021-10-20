04b745d1ff61a1cc5ebce44c34b258bfec672b55
tech-ticks <65084428+tech-ticks@users.noreply.github.com>
2021-10-21 01:02:44 +0200

    Fix unrecognized dylib names in Mac build (#108)
    * Fix unrecognized dylib names in Mac build
    
    * Fix wrong Mac application data folder returned by .NET

--- SkyEditor.UI/Infrastructure/Settings.cs
+++ SkyEditor.UI/Infrastructure/Settings.cs
@@ -6,92 +6,103 @@ using Newtonsoft.Json;
 
 namespace SkyEditorUI.Infrastructure
 {
-  public enum BuildFileStructureType
-  {
-    Atmosphere,
-    Emulator
-  }
-
-  public class Settings
-  {
-
-    public static string DataPath =>
-      Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DreamNexus");
-
-    public static string SettingsFilePath => Path.Combine(DataPath, "settings.json");
-
-    public string? SwitchIp { get; set; }
-    public string? SwitchFtpPort { get; set; }
-    public string? SwitchFtpUser { get; set; }
-    public string? SwitchFtpPassword { get; set; }
-
-    public string? RtdxRomPath { get; set; }
-    public BuildFileStructureType BuildFileStructure { get; set; } = BuildFileStructureType.Atmosphere;
-
-    public bool EnableHactoolLogging { get; set; } = true;
-
-    public List<(string nameOrId, string path)> RecentModpacks { get; set; } = new List<(string nameOrId, string path)>();
-
-    public static Settings Load()
+    public enum BuildFileStructureType
     {
-      try
-      {
-        return TryLoad();
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine($"Failed to load settings: {e}");
-        return new Settings();
-      }
+        Atmosphere,
+        Emulator
     }
 
-    public static Settings TryLoad()
+    public class Settings
     {
-      if (!Directory.Exists(SettingsFilePath))
-      {
-        Directory.CreateDirectory(DataPath);
-      }
-      if (!File.Exists(SettingsFilePath))
-      {
-        return new Settings();
-      }
 
-      var settingsJson = File.ReadAllText(SettingsFilePath);
-      var deserializedSettings = JsonConvert.DeserializeObject<Settings>(settingsJson);
-      if (deserializedSettings == null)
-      {
-        Console.Error.WriteLine("Deserialized settings were null, this should never happen! Creating new Settings.");
-        return new Settings();
-      }
+        public static string DataPath
+        {
+            get
+            {
+                // The application data folder returned on Mac is just flat out wrong
+                // https://github.com/dotnet/runtime/issues/40353
+                var appDataFolder = OperatingSystem.IsMacOS()
+                    ? Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library/Application Support")
+                    : Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
-      return deserializedSettings;
+                return Path.Combine(appDataFolder, "DreamNexus");
+            }
+        }
+        public static string SettingsFilePath => Path.Combine(DataPath, "settings.json");
+
+        public string? SwitchIp { get; set; }
+        public string? SwitchFtpPort { get; set; }
+        public string? SwitchFtpUser { get; set; }
+        public string? SwitchFtpPassword { get; set; }
+
+        public string? RtdxRomPath { get; set; }
+        public BuildFileStructureType BuildFileStructure { get; set; } = BuildFileStructureType.Atmosphere;
+
+        public bool EnableHactoolLogging { get; set; } = true;
+
+        public List<(string nameOrId, string path)> RecentModpacks { get; set; } = new List<(string nameOrId, string path)>();
+
+        public static Settings Load()
+        {
+            try
+            {
+                return TryLoad();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to load settings: {e}");
+                return new Settings();
+            }
+        }
+
+        public static Settings TryLoad()
+        {
+            if (!Directory.Exists(SettingsFilePath))
+            {
+                Directory.CreateDirectory(DataPath);
+            }
+            if (!File.Exists(SettingsFilePath))
+            {
+                return new Settings();
+            }
+
+            var settingsJson = File.ReadAllText(SettingsFilePath);
+            var deserializedSettings = JsonConvert.DeserializeObject<Settings>(settingsJson);
+            if (deserializedSettings == null)
+            {
+                Console.Error.WriteLine("Deserialized settings were null, this should never happen! Creating new Settings.");
+                return new Settings();
+            }
+
+            return deserializedSettings;
+        }
+
+        public void Save()
+        {
+            if (!Directory.Exists(DataPath))
+            {
+                Directory.CreateDirectory(DataPath);
+            }
+
+            var serialized = JsonConvert.SerializeObject(this, Formatting.Indented);
+            if (serialized != null)
+            {
+                File.WriteAllText(SettingsFilePath, serialized);
+            }
+        }
+
+        public void AddRecentModpack(string nameOrId, string path)
+        {
+            if (!RecentModpacks.Any(modpack => modpack.path == path))
+            {
+                RecentModpacks.Insert(0, (nameOrId, path));
+            }
+        }
+
+        public bool FtpSettingsComplete() => !string.IsNullOrWhiteSpace(SwitchIp)
+            && !string.IsNullOrWhiteSpace(SwitchFtpPort)
+            && !string.IsNullOrWhiteSpace(SwitchFtpUser)
+            && !string.IsNullOrWhiteSpace(SwitchFtpPassword);
     }
-
-    public void Save()
-    {
-      if (!Directory.Exists(DataPath))
-      {
-        Directory.CreateDirectory(DataPath);
-      }
-
-      var serialized = JsonConvert.SerializeObject(this, Formatting.Indented);
-      if (serialized != null)
-      {
-        File.WriteAllText(SettingsFilePath, serialized);
-      }
-    }
-
-    public void AddRecentModpack(string nameOrId, string path)
-    {
-      if (!RecentModpacks.Any(modpack => modpack.path == path))
-      {
-        RecentModpacks.Insert(0, (nameOrId, path));
-      }
-    }
-
-    public bool FtpSettingsComplete() => !string.IsNullOrWhiteSpace(SwitchIp)
-      && !string.IsNullOrWhiteSpace(SwitchFtpPort)
-      && !string.IsNullOrWhiteSpace(SwitchFtpUser)
-      && !string.IsNullOrWhiteSpace(SwitchFtpPassword);
-  }
 }
